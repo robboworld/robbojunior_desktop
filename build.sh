a475757bce2bb4d6f312a3dd2be3561758a69cd6
Yarila <yarila682@yandex.ru>
2017-12-25 14:04:48 +0300

    add build_distribution_package folder and update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
+echo 'npm run build'
+
 npm run build
+
+echo 'rm app.bundle.js'
 rm app.bundle.js
+
+echo 'rm app.bundle.js.map'
 rm app.bundle.js.map
 
+echo 'cp ./src/build/bundles/app.bundle.js      ./'
 cp ./src/build/bundles/app.bundle.js      ./
+
+echo 'cp ./src/build/bundles/app.bundle.js.map  ./'
 cp ./src/build/bundles/app.bundle.js.map  ./ 
 
+echo 'rm ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js'
+rm ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js
+
+echo 'rm ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js.map'
+rm ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js.map
+
+echo 'cp ./app.bundle.js      ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/'
+cp ./app.bundle.js      ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/
+
+echo 'cp ./app.bundle.js.map  ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/ '
+cp ./app.bundle.js.map  ./build_ditribution_package/linux/robbojunior/opt/junior/jr/resources/app/ 
+
+echo 'rm ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/app.bundle.js'
+rm ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/app.bundle.js
+
+echo 'rm ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/app.bundle.js.map'
+rm ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/app.bundle.js.map
+
+echo 'cp ./app.bundle.js      ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/'
+cp ./app.bundle.js      ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/
+
+echo 'cp ./app.bundle.js.map  ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/ '
+cp ./app.bundle.js.map  ./build_ditribution_package/linux/robbojunior-x32/opt/junior/jr/resources/app/ 
+
+
+echo 'npm run start'
 npm run start
