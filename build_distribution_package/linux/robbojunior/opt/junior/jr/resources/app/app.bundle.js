98e94daeba4719b51a4e908580da33301dd2c2bf
Yarila <yarila682@yandex.ru>
2018-02-12 14:22:40 +0300

    svglibrary update

--- build_distribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js
+++ build_distribution_package/linux/robbojunior/opt/junior/jr/resources/app/app.bundle.js
@@ -7055,6 +7055,12 @@
 	            tabletInterface.recordsound_recordclose = function () {
 	               console.log("recordsound_recordclose");
 	
+	               if (tabletInterface.record.record_stop_time < 0 || tabletInterface.record.record_stop_time == null || typeof tabletInterface.record.record_stop_time == 'undefined') {
+	
+	                  tabletInterface.record.record_stop_time = Date.now();
+	                  console.log("record stoped at: " + tabletInterface.record.record_stop_time + " ms");
+	               }
+	
 	               tabletInterface.record.record_duration = tabletInterface.record.record_stop_time - tabletInterface.record.record_start_time;
 	               console.log("record duration: " + tabletInterface.record.record_duration + " ms");
 	
@@ -7066,6 +7072,8 @@
 	               });
 	
 	               tabletInterface.io_setmedianame(tabletInterface.audioData, tabletInterface.audioName, "wav");
+	
+	               return tabletInterface.record;
 	            };
 	
 	            tabletInterface.scratchjr_choosecamera = function () {
@@ -8170,6 +8178,8 @@
 	
 	var customSounds = [];
 	
+	var recordedSounds = [];
+	
 	var custom_sound_index = 0;
 	
 	var ScratchAudio = function () {
@@ -8178,6 +8188,27 @@
 	    }
 	
 	    _createClass(ScratchAudio, null, [{
+	        key: 'copyRecordedSounds',
+	        value: function copyRecordedSounds(sounds_arr) {
+	
+	            sounds_arr.forEach(function (entry) {
+	
+	                recordedSounds.push(entry);
+	                ScratchAudio.loadFromLocal('', entry.sound_name);
+	            });
+	        }
+	    }, {
+	        key: 'addRecordedSound',
+	        value: function addRecordedSound(sound_name, sound_duration) {
+	
+	            var sound_object = {};
+	
+	            sound_object.sound_name = sound_name;
+	            sound_object.sound_duration = sound_duration;
+	
+	            recordedSounds.push(sound_object);
+	        }
+	    }, {
 	        key: 'sndFX',
 	        value: function sndFX(name) {
 	            ScratchAudio.sndFXWithVolume(name, 1.0);
@@ -8298,6 +8329,11 @@
 	        key: 'customSounds',
 	        get: function get() {
 	            return customSounds;
+	        }
+	    }, {
+	        key: 'recordedSounds',
+	        get: function get() {
+	            return recordedSounds;
 	        }
 	    }]);
 	
@@ -9318,6 +9354,9 @@
 	            for (var i = 0; i < pages.length; i++) {
 	                Project.recreatePage(pages[i], data[pages[i]]);
 	            }
+	
+	            ScratchAudio.copyRecordedSounds(data.sounds); //modified_by_Yaroslav
+	
 	            mediaCountBase = mediaCount;
 	        }
 	    }, {
@@ -9488,6 +9527,10 @@
 	            for (var i = 0; i < _ScratchJr2.default.stage.pages.length; i++) {
 	                obj[_ScratchJr2.default.stage.pages[i].id] = _ScratchJr2.default.stage.pages[i].encodePage();
 	            }
+	
+	            //modified_by_Yaroslav
+	            obj.sounds = ScratchAudio.recordedSounds;
+	
 	            return obj;
 	        }
 	    }, {
@@ -9778,7 +9821,7 @@
 	var currentProject = undefined;
 	var editmode = void 0;
 	
-	var isDebugging = false;
+	var isDebugging = true; //modified_by_Yaroslav
 	var time = void 0;
 	var userStart = false;
 	var onHold = false;
@@ -22577,7 +22620,8 @@
 	        key: 'playSound',
 	        value: function playSound(strip) {
 	            var b = strip.thisblock;
-	            var name = b.getSoundName(strip.spr.sounds);
+	            //  var name = b.getSoundName(strip.spr.sounds); //modified_by_Yaroslav
+	            var name = b.getSoundName(_ScratchAudio2.default.recordedSounds);
 	            //	console.log ('playSound', name);
 	            if (!strip.audio) {
 	                var snd = _ScratchAudio2.default.projectSounds[name];
@@ -26138,6 +26182,8 @@
 	        key: 'cut_extension',
 	        value: function cut_extension(file_name) {
 	
+	            if (typeof file_name != "string") return "Speaker";
+	
 	            var extensions = [".wav", ".webm", ".mp3"];
 	
 	            for (var i = 0; i < extensions.length; i++) {
@@ -26364,14 +26410,25 @@
 	    }, {
 	        key: 'getSoundName',
 	        value: function getSoundName(list) {
+	            /*  var val = this.arg.argValue;
+	              if (Number(val).toString() == 'NaN') {
+	                  return val;
+	              }
+	              if (list.length <= val) {
+	                  return list[0];
+	              }
+	              return list[val]; */
+	
+	            //modified_by_Yaroslav
+	
 	            var val = this.arg.argValue;
+	
 	            if (Number(val).toString() == 'NaN') {
 	                return val;
+	            } else {
+	
+	                return list[val].sound_name;
 	            }
-	            if (list.length <= val) {
-	                return list[0];
-	            }
-	            return list[val];
 	        }
 	    }, {
 	        key: 'update',
@@ -29564,12 +29621,13 @@
 	        }
 	    }, {
 	        key: 'registerProjectSound',
-	        value: function registerProjectSound() {
+	        value: function registerProjectSound(record) {
 	            function whenDone(snd) {
 	                if (snd != 'error') {
 	                    var spr = _ScratchJr2.default.getSprite();
 	                    var page = spr.div.parentNode.owner;
 	                    spr.sounds.push(recordedSound);
+	                    _ScratchAudio2.default.addRecordedSound(recordedSound, record.record_duration); //modified_by_Yaroslav
 	                    _Undo2.default.record({
 	                        action: 'recordsound',
 	                        who: spr.id,
@@ -29710,6 +29768,10 @@
 	var _Vector = __webpack_require__(19);
 	
 	var _Vector2 = _interopRequireDefault(_Vector);
+	
+	var _ScratchAudio = __webpack_require__(10);
+	
+	var _ScratchAudio2 = _interopRequireDefault(_ScratchAudio);
 	
 	var _lib = __webpack_require__(1);
 	
@@ -30098,6 +30160,7 @@
 	                }
 	            }
 	            data.layers = layers;
+	
 	            return data;
 	        }
 	    }, {
@@ -34931,11 +34994,16 @@
 	                        }
 	                        break;
 	                    case 'playusersnd':
-	                        if (this.spr.sounds.length <= list[i][1]) {
-	                            list[i][0] = 'playsnd';
-	                            //AZ TODO
-	                            //                    list[i][1] = this.spr.sounds[0];
-	                        }
+	                        /*  if (this.spr.sounds.length <= list[i][1]) {
+	                              list[i][0] = 'playsnd';
+	                        //AZ TODO
+	                        //                    list[i][1] = this.spr.sounds[0];
+	                          }*/
+	
+	                        //modified_by_Yaroslav
+	
+	                        list[i][0] = 'playusersnd';
+	
 	                        break;
 	                    case 'playsnd':
 	                        var snd = this.spr.sounds.indexOf(list[i][1]);
