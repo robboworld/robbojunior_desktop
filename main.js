a7758237a0565ad87d4526b5419dd3b0541e261f
Yarila682 <yarila682@yandex.ru>
2022-08-25 19:49:31 +0100

    Support for new Electron

--- main.js
+++ main.js
@@ -13,7 +13,15 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow(
+  {
+   webPreferences: {
+       nodeIntegration: true,
+       contextIsolation: false, 
+       width: 800, 
+       height: 600 
+   }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
